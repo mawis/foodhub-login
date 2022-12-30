d6739615d7c169519c63e10a1727b05b224ea024
Matthias Wimmer <m@tthias.eu>
2022-12-30 23:40:00 +0100

    Start OAuth 2.0
    - Start OAuth 2.0 with the authorization code grant
    - Fetch token from the token endpoint
    - Print server errors to the user

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,20 @@ use rocket::{routes, Build, Rocket};
 use rocket_cors::CorsOptions;
 use rocket_include_tera::{tera_resources_initialize, TeraResponse};
 use rocket_sync_db_pools::database;
+use serde_derive::Deserialize;
 use uuid::Uuid;
 
 mod web;
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct Config {
+    authorization_endpoint: String,
+    token_endpoint: String,
+    client_id: String,
+    client_secret: String,
+    redirect_uri: String,
+}
 
 #[database("postgres_diesel")]
 pub struct DbConn(diesel::PgConnection);
@@ -35,15 +46,24 @@ fn rocket() -> Rocket<Build> {
     rocket::build()
         .attach(DbConn::fairing())
         .attach(AdHoc::on_ignite("Diesel Migrations", run_migrations))
+        .attach(AdHoc::config::<Config>())
         .attach(CorsOptions::default().to_cors().unwrap())
         .attach(TeraResponse::fairing(|tera| {
             tera_resources_initialize!(
                 tera,
                 "base" => "views/base.tera",
-                "login" => "views/login.tera"
+                "login" => "views/login.tera",
+                "autherror" => "views/autherror.tera"
             )
         }))
-        .mount("/login", routes![web::login::get_index])
+        .mount(
+            "/login",
+            routes![
+                web::login::get_index,
+                web::login::post_index,
+                web::login::get_index_with_code
+            ],
+        )
 }
 
 #[rocket::main]
--- src/web/login.rs
+++ src/web/login.rs
@@ -1,12 +1,164 @@
-use rocket::{get, State};
+use crate::Config;
+use log::{error, info};
+use reqwest::StatusCode;
+use rocket::form::{Form, FromForm};
+use rocket::http::uri::Uri;
+use rocket::http::{Cookie, CookieJar};
+use rocket::response::Redirect;
+use rocket::{get, post, uri, State};
 use rocket_include_tera::{tera_response, EtagIfNoneMatch, TeraContextManager, TeraResponse};
+use serde::Deserialize;
 use std::collections::HashMap;
+use thiserror::Error;
 
-#[get("/")]
+#[derive(FromForm, Debug)]
+pub struct LoginForm {
+    privacy_policy: Option<String>,
+}
+
+#[derive(Deserialize)]
+pub struct TokenResponse {
+    access_token: String,
+    token_type: String,
+}
+
+#[derive(Error, Debug)]
+pub enum AuthServerError {
+    #[error("reqwest error")]
+    ReqwestError(#[from] reqwest::Error),
+
+    #[error("HTTP state error")]
+    HttpStateError { state: StatusCode, body: String },
+}
+
+#[get("/?<code>")]
+pub async fn get_index_with_code(
+    code: &str,
+    tera_cm: &State<TeraContextManager>,
+    etag_if_none_match: EtagIfNoneMatch<'_>,
+    cookies: &CookieJar<'_>,
+    config: &State<Config>,
+) -> Result<Redirect, TeraResponse> {
+    info!("Code is {:?}", code);
+
+    let token = get_access_token(config, code).await;
+
+    match token {
+        Err(AuthServerError::HttpStateError { state, body }) => {
+            let mut context = HashMap::new();
+            context.insert("code", state.as_str().to_string());
+            context.insert("body", body);
+            Err(tera_response!(
+                tera_cm,
+                etag_if_none_match,
+                "autherror",
+                context
+            ))
+        }
+        Err(AuthServerError::ReqwestError(_)) => {
+            let mut context = HashMap::new();
+            context.insert("code", String::new());
+            context.insert("body", format!("{:?}", token));
+            Err(tera_response!(
+                tera_cm,
+                etag_if_none_match,
+                "autherror",
+                context
+            ))
+        }
+        Ok(token) => {
+            info!("got token: {}", token);
+            let mut context = HashMap::<&str, &str>::new();
+            Err(tera_response!(
+                tera_cm,
+                etag_if_none_match,
+                "login",
+                context
+            ))
+        }
+    }
+}
+
+#[get("/", rank = 1)]
 pub async fn get_index(
     tera_cm: &State<TeraContextManager>,
     etag_if_none_match: EtagIfNoneMatch<'_>,
 ) -> TeraResponse {
     let context = HashMap::<String, String>::new();
     tera_response!(tera_cm, etag_if_none_match, "login", context)
+}
+
+#[post("/", data = "<form>")]
+pub async fn post_index(
+    tera_cm: &State<TeraContextManager>,
+    etag_if_none_match: EtagIfNoneMatch<'_>,
+    form: Form<LoginForm>,
+    cookies: &CookieJar<'_>,
+    config: &State<Config>,
+) -> Result<Redirect, TeraResponse> {
+    match form.privacy_policy == Some("accepted".to_string()) {
+        true => {
+            let policy_accepted_cookie = Cookie::build("privacy_policy", "accepted")
+                .path("/")
+                .secure(true)
+                .finish();
+            cookies.add(policy_accepted_cookie);
+            Ok(Redirect::to(start_oauth_uri(config)))
+        }
+        false => {
+            let mut context = HashMap::<&str, &str>::new();
+            context.insert("checkbox_class", "invalid-feedback");
+            Err(tera_response!(
+                tera_cm,
+                etag_if_none_match,
+                "login",
+                context
+            ))
+        }
+    }
+}
+
+async fn get_access_token(config: &Config, code: &str) -> Result<String, AuthServerError> {
+    let client = reqwest::Client::builder()
+        .cookie_store(false)
+        .user_agent("Schichtvertretung")
+        .build()?;
+    let body = token_body(config, code);
+    info!("Body: {}", body);
+    let res = client
+        .post(&config.token_endpoint)
+        .body(body)
+        .send()
+        .await?;
+
+    let status = res.status();
+    if status != StatusCode::OK {
+        return Err(AuthServerError::HttpStateError {
+            state: status,
+            body: res.text().await?,
+        });
+    }
+
+    //let token: TokenResponse = res.json().await?;
+
+    Ok(String::from("got token!"))
+}
+
+fn start_oauth_uri(config: &Config) -> String {
+    format!(
+        "{}?response_type=code&client_id={}&redirect_uri={}&scope=openid&state=foo",
+        &config.authorization_endpoint,
+        &config.client_id,
+        urlencoding::encode(&config.redirect_uri)
+    )
+}
+
+fn token_body(config: &Config, code: &str) -> String {
+    format!(
+        "grant_type=authorization_code&code={}&client_id={}&redirect_uri={}&client_secret={}",
+        code,
+        &config.client_id,
+        urlencoding::encode(&config.redirect_uri),
+        &config.client_secret
+    )
 }
