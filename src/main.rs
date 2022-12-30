bf0f77c14efe11c9d36f49f5a660cc09712b8519
Matthias Wimmer <m@tthias.eu>
2022-12-30 23:49:57 +0100

    No database needed

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-#[macro_use]
-extern crate diesel;
-#[macro_use]
-extern crate diesel_migrations;
-
 use rocket::fairing::AdHoc;
 use rocket::{routes, Build, Rocket};
 use rocket_cors::CorsOptions;
 use rocket_include_tera::{tera_resources_initialize, TeraResponse};
-use rocket_sync_db_pools::database;
 use serde_derive::Deserialize;
 use uuid::Uuid;
 
@@ -23,29 +17,12 @@ pub struct Config {
     redirect_uri: String,
 }
 
-#[database("postgres_diesel")]
-pub struct DbConn(diesel::PgConnection);
-
 pub fn uuid_convert(ruuid: rocket::serde::uuid::Uuid) -> Uuid {
     Uuid::from_bytes(ruuid.into_bytes())
 }
 
-embed_migrations!();
-
-async fn run_migrations(rocket: Rocket<Build>) -> Rocket<Build> {
-    let conn = DbConn::get_one(&rocket)
-        .await
-        .expect("Could not get database connection");
-    conn.run(|c| embedded_migrations::run(c))
-        .await
-        .expect("Could not run migrations");
-    rocket
-}
-
 fn rocket() -> Rocket<Build> {
     rocket::build()
-        .attach(DbConn::fairing())
-        .attach(AdHoc::on_ignite("Diesel Migrations", run_migrations))
         .attach(AdHoc::config::<Config>())
         .attach(CorsOptions::default().to_cors().unwrap())
         .attach(TeraResponse::fairing(|tera| {
